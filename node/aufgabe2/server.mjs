30f33020fd34f3153ff300164ff67ec822dd55ec
maurozappa <fabian_mueller7@sluz.ch>
2022-01-26 11:09:09 +0100

    Aufgabe 1-3

---
+++ node/aufgabe2/server.mjs
@@ -0,0 +1,14 @@
+import { createServer } from 'http';
+import { readFile } from 'fs';
+
+
+readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(8080);
+});
